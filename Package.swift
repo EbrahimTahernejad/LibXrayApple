9e456818ed172488561381aab026f46531f24a0b
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2024-09-12 18:09:36 +0000

    LibXray Release 0.0.2

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "LibXray",
-      url: "https://github.com/EbrahimTahernejad/LibXrayApple/releases/download/0.0.1/LibXray.xcframework.zip",
-      checksum: "5018ec53c1a706dcf38132586b25f949106b4972f31b9abf6720c1c05b0933f0"
+      url: "https://github.com/EbrahimTahernejad/LibXrayApple/releases/download/0.0.2/LibXray.xcframework.zip",
+      checksum: "d8e1c932e7cd545f707a908266bfc52b8cf528f66ae5873b9644575f21051ac7"
     )
   ]
 )
